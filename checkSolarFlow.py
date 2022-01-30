c0415ab6f789793d87799b89fd6f20fad123e7a9
peterschmid <peter.r.schmid@gmail.com>
2022-01-30 17:09:39 +0100

    fixed checkSolarFlow.py

--- checkSolarFlow.py
+++ checkSolarFlow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, smtplib
+import sys, smtplib, subprocess
 
 if len(sys.argv) != 2:
     print("Use first Argument as data file name")
@@ -47,32 +47,8 @@ def sendMail(sub, text):
   server.quit()
 
 def tail( filename, lines=20 ):
-    total_lines_wanted = lines
-
-    f = open(filename, "r")
-    BLOCK_SIZE = 1024
-    f.seek(0, 2)
-    block_end_byte = f.tell()
-    lines_to_go = total_lines_wanted
-    block_number = -1
-    blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting
-                # from the end of the file
-    while lines_to_go > 0 and block_end_byte > 0:
-        if (block_end_byte - BLOCK_SIZE > 0):
-            # read the last block we haven't yet read
-            f.seek(block_number*BLOCK_SIZE, 2)
-            blocks.append(f.read(BLOCK_SIZE))
-        else:
-            # file too small, start from begining
-            f.seek(0,0)
-            # only read what was not read
-            blocks.append(f.read(block_end_byte))
-        lines_found = blocks[-1].count('\n')
-        lines_to_go -= lines_found
-        block_end_byte -= BLOCK_SIZE
-        block_number -= 1
-    all_read_text = ''.join(reversed(blocks))
-    return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
+    output = subprocess.check_output(['tail', '-n', str(lines), filename])
+    return output.decode('ascii')
 
 def toNumbers(values):
     values2 = []
